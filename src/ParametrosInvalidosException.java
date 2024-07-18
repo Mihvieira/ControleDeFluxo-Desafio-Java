e4581d9f5149c8aca7690f253c9f77b7b1cac35a
Mirelly Almeida <136247614+Mihvieira@users.noreply.github.com>
2024-07-18 17:59:11 -0400

    desafio concluído

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,10 @@
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String string) {
+        super(string);
+    }
+
+    public static void main(String message){
+        
+    }
+}
